7ed74d40f3acedabf996fdd9b0c9bfa69c4b1515
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-05 17:20:08 +0700

    update_multiple_products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,9 +4,21 @@ class ProductsController < ApplicationController
   end
 
   def edit_multiple
-    @products = Product.where(id: params[:product_ids])
+    @products = Product.includes(:category).where(id: params[:product_ids])
   end
 
   def update_multiple
+    @products = Product.where(id: params[:product_ids].split)
+    @products.each do |product|
+      product.update_attributes product_params
+    end
+
+    redirect_to root_url
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :category_id, :price)
   end
 end
